66ff983fcb4f7daf8a108f6467bb592eff4db935
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2011-02-18 09:32:50 +0100

    improved login and form
    - the login form now takes extensions registered for a viewlet manager
    
    - the form now displays input format description and example
    
    - improved css

--- src/quotationtool/skin/browser/login.py
+++ src/quotationtool/skin/browser/login.py
@@ -1,3 +1,8 @@
+# The login and logout pages are defined in the subpackage
+# quotationtool.skin.browser.authetication. In this module we only
+# define extensions to the login form and other login related
+# components used in the skin.
+#
 # Extensions to the login form (e.g. a link to a signuo form) may be
 # registered for a viewlet manager that implements
 # quotationtool.skin.interfaces.ILoginFormExtensions
@@ -7,7 +12,7 @@ from zope.viewlet.manager import ViewletManager, WeightOrderedViewletManager
 from quotationtool.skin import interfaces
 
 
-LoginFormExtensions = ViewletManager('loginformextensions',
+LoginFormExtensions = ViewletManager('loginform_extensions',
                                      interfaces.ILoginFormExtensions,
                                      bases = (WeightOrderedViewletManager,),
                                      )
