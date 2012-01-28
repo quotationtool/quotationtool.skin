9644835aa41de58dc3aedcdf0e88ba6dbc5034cd
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2012-01-28 01:36:48 +0100

    Improved CSS and namespaceadapter

--- src/quotationtool/skin/browser/tales.py
+++ src/quotationtool/skin/browser/tales.py
@@ -23,7 +23,7 @@ class QuotationtoolTalesAPI(object):
 
     @property
     def creator(self):
-        """ Returns the title for the context item. Returns the real
+        """ Returns the creator for the context item. Returns the real
         title given in the pau, not the dc.creator string, which is an
         internal computed value."""
         creator = u"Unkown"
@@ -37,6 +37,26 @@ class QuotationtoolTalesAPI(object):
         if pau is not None:
             try:
                 creator = pau.getPrincipal(dc.creators[0]).title
+            except Exception:
+                pass
+        return creator
+
+    @property
+    def editor(self):
+        """ Returns the creator for the context item. Returns the real
+        title given in the pau, not the dc.creator string, which is an
+        internal computed value."""
+        creator = u"Unkown"
+        dc = IZopeDublinCore(self.context, None)
+        if dc is None:
+            return creator
+        pau = zope.component.queryUtility(
+            IAuthentication,
+            context = hooks.getSite()
+            )
+        if pau is not None:
+            try:
+                creator = pau.getPrincipal(dc.creators[-1]).title
             except Exception:
                 pass
         return creator
