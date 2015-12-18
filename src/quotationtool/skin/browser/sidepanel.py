b8ffc82d4e8883226978dd8cdd41962da3450eea
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2015-12-18 22:26:52 +0100

    sidepanel ready for use
    - layout: div.sidepanel is only present, if there is at least one
      viewlet in the sidepanel
    
    - the sidepanels template is now defined in zcml only
    
    - css: sidepanel floats right, using CSS3 :nth-child
    
    - css: sidepanel can be styled just like content and contentlabel
    
    - css: added reset for quirk-modes box-model

--- src/quotationtool/skin/browser/sidepanel.py
+++ src/quotationtool/skin/browser/sidepanel.py
@@ -1,14 +1,8 @@
 from zope.viewlet.manager import ViewletManager, WeightOrderedViewletManager
-import os.path
 
 from quotationtool.skin import interfaces
 
-
-sidepanel_template = os.path.join(os.path.dirname(__file__), 'sidepanel.pt')
-
-
 SidePanel = ViewletManager('sidepanel',
                            interfaces.ISidePanel,
-                           template = sidepanel_template,
                            bases = (WeightOrderedViewletManager,))
 
